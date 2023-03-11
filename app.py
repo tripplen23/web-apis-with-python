a92a355ce78c787e8eacb3db4bf5bad75e6d494c
tripplen23 <tripplen63@gmail.com>
2023-03-11 13:06:43 +0700

    Learning Python Flask 2

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
+# Import the redirect() method from "flask" library to enable us to redirect to another URL
 from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
 @app.get("/")
 def index():
-    """
-    TODO: Render the home page provided under templates/index.html in the repository
-    """
-    return "TODO"
-
+    #Render the index.html template and return it
+    return render_template("index.html")
+# TODO: Define the login of the route "/search", that will be used to listen and response to GET requests
 @app.get("/search")
 def search():
-	"""
-	TODO:
-	1. Capture the word that is being searched
-	2. Seach for the word on Google and display results
-	"""
-    return "TODO"
-
+    #parse the arguments "q" from the incoming GET request querystring
+    args = request.args.get("q")
+    #redirect to Google website with the querystring of Google search and adding the argument we defined above
+    return redirect(f"https://www.google.com/search?q= {args} " )
 if __name__ == "__main__":
     app.run()
